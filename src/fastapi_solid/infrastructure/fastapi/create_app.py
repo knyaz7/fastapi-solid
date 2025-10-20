67b661b5b995f9eeccae05b96fe9f6aa6215033c
knyaz7 <vn.guitar@mail.ru>
2025-10-20 19:59:59 +0300

    chore: middleware setup

--- src/fastapi_solid/infrastructure/fastapi/create_app.py
+++ src/fastapi_solid/infrastructure/fastapi/create_app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
@@ -24,6 +25,15 @@ async def lifespan(app: FastAPI):
 
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan, docs_url="/api/docs")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    
     app.include_router(api_v1_router)
     register_error_handlers(app)
     return app
